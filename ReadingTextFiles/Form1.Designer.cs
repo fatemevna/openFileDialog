da5d31dfbb3f3437476e3c74b9911ee9266c16e1
fatemevna <mohajerf43@gmail.com>
2024-08-07 14:53:08 +0330

    Project Created

---
+++ ReadingTextFiles/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ReadingTextFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbWrite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(346, 165);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbWrite
+            // 
+            this.tbWrite.Location = new System.Drawing.Point(166, 213);
+            this.tbWrite.Multiline = true;
+            this.tbWrite.Name = "tbWrite";
+            this.tbWrite.Size = new System.Drawing.Size(482, 200);
+            this.tbWrite.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbWrite);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbWrite;
+    }
+}
+
---
+++ ReadingTextFiles/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadingTextFiles
+{
+    public partial class Form1 : Form
+    {
+        int result;
+        int operand1;
+       int operand2;
+        
+        string firstLine,secondLine,thirdLine;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            string pathToFile = "";//to save the location of the selected object
+                                   // private void openToolStripMenuItem_Click(object sender, EventArgs e)
+                                   //{
+            //string text = "";
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(theDialog.FileName.ToString());
+                pathToFile = theDialog.FileName;//doesn't need .tostring because .filename returns a string// saves the location of the selected object
+
+            }
+
+            if (File.Exists(pathToFile))// only executes if the file at pathtofile exists//you need to add the using System.IO reference at the top of te code to use this
+            {
+
+
+                //method1
+                 firstLine = File.ReadAllLines(pathToFile).Skip(0).Take(1).First();//selects first line of the file
+                 secondLine = File.ReadAllLines(pathToFile).Skip(1).Take(1).First();
+                 thirdLine = File.ReadAllLines(pathToFile).Skip(2).Take(1).First();
+                 operand1 = Int32.Parse(firstLine);
+                 operand2 = Int32.Parse(secondLine);
+                if (thirdLine == "+")
+                {
+                    result = operand1 + operand2;
+                }
+                else if (thirdLine == "-")
+                {
+                    result = operand1 - operand2;
+                }
+                else if (thirdLine == "*")
+                {
+                    result = operand1 * operand2;
+                }
+                else if (thirdLine == "/")
+                {
+                    result = operand1 / operand2;
+                }
+
+            }
+            tbWrite.Text = firstLine+" "+secondLine+"\n" + operand1 + " " + thirdLine + " " + operand2 + " " + "=" + " " + result;
+        }
+    
+    }
+}
